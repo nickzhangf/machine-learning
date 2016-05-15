6230ab27bc4bee4c83412098869024c8ccf01716
nickzhangf <fengz2008@163.com>
2016-05-15 09:04:18 +0800

    计算信息熵

---
+++ src/algorithm/trees.py
@@ -0,0 +1,25 @@
+from math import log
+
+def calcShannonEnt(dataSet):
+    numEntries = len(dataSet)
+    labelCounts = { }
+
+    for featVec in dataSet:
+        currentLabel = featVec[-1]
+        labelCounts[currentLabel] = labelCounts.get(currentLabel, 0) + 1
+    shannonEnt = 0.0
+    for key in labelCounts:
+        prob = float(labelCounts[key])/numEntries
+        shannonEnt -= prob * log(prob, 2)
+
+    return shannonEnt
+
+def createDataSet():
+    dataSet = [[1, 1, 'yes'],
+               [1, 1, 'yes'],
+               [1, 0, 'no'],
+               [0, 1, 'no'],
+               [0, 1, 'no']]
+    labels = ['no surfacing','flippers']
+    return dataSet, labels
+
